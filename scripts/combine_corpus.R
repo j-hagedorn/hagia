0c02e0097a1a005be0c2aeba9f7c15f611d4c1a7
Joshh <j-hagedorn@users.noreply.github.com>
2026-09-07 21:26:24 -0400

    Update combine_corpus.R

--- scripts/combine_corpus.R
+++ scripts/combine_corpus.R
@@ -1,11 +1,32 @@
 library(dplyr)
 library(purrr)
 
+# Load all scraped dataset files
+df_ado_pl123 <- arrow::read_parquet("derived/df_ado_pl123.parquet")
+df_aelfric <- arrow::read_parquet("derived/df_aelfric.parquet")
+df_doaks <- arrow::read_parquet("derived/df_doaks.parquet")
+df_eusebius_martyrs <- arrow::read_parquet("derived/df_eusebius_martyrs.parquet")
+df_golden_legend <- arrow::read_parquet("derived/df_golden_legend.parquet")
+df_historia_tripartita <- arrow::read_parquet("derived/df_historia_tripartita.parquet")
+df_jean_de_mailly <- arrow::read_parquet("derived/df_jean_de_mailly.parquet")
+df_sozomen <- arrow::read_parquet("derived/df_sozomen.parquet")
+df_theodoret_eh <- arrow::read_parquet("derived/df_theodoret_eh.parquet")
+df_theodoret <- arrow::read_parquet("derived/df_theodoret.parquet")
+df_npnf <- arrow::read_parquet("derived/df_npnf.parquet")
+
+
 datasets <- list(
   aelfric = df_aelfric,
   doaks = df_doaks,
   golden_legend = df_golden_legend,
-  npnf = df_npnf
+  npnf = df_npnf,
+  theodoret = df_theodoret,
+  eusebius_martyrs = df_eusebius_martyrs,
+  jean_de_mailly = df_jean_de_mailly,
+  sozomen = df_sozomen,
+  theodoret_eh = df_theodoret_eh,
+  ado_pl123 = df_ado_pl123,
+  historia_tripartita = df_historia_tripartita
 ) |>
   imap(
     ~ mutate(
